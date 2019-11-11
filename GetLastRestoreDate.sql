461c18411fb0ee657901365a84f1d56bb10a65be
Tyrell66 <3510934+Tyrell66@users.noreply.github.com>
2019-11-11 09:05:35 +0100

    Update GetLastRestoreDate.sql

--- GetLastRestoreDate.sql
+++ GetLastRestoreDate.sql
@@ -1,15 +1,34 @@
-WITH LastRestores AS
+WITH MostRecentRestore AS
 (
-SELECT
-    DatabaseName = [d].[name] ,
-    [d].[create_date] ,
-    [d].[compatibility_level] ,
-    [d].[collation_name] ,
-    r.*,
-    RowNum = ROW_NUMBER() OVER (PARTITION BY d.Name ORDER BY r.[restore_date] DESC)
-FROM master.sys.databases d
-LEFT OUTER JOIN msdb.dbo.[restorehistory] r ON r.[destination_database_name] = d.Name
+SELECT 
+RowNum = ROW_NUMBER() OVER (PARTITION BY RH.Destination_database_name ORDER BY RH.Restore_Date DESC),
+RH.Restore_date, 
+BS.[database_name] as Source_Database, 
+RH.Destination_Database_Name, 
+BS.Backup_Start_Date, 
+BS.Backup_Finish_Date, 
+CASE WHEN RH.restore_type = 'D' THEN 'Database'
+  WHEN RH.restore_type = 'F' THEN 'File'
+  WHEN RH.restore_type = 'G' THEN 'Filegroup'
+  WHEN RH.restore_type = 'I' THEN 'Differential'
+  WHEN RH.restore_type = 'L' THEN 'Log'
+  WHEN RH.restore_type = 'V' THEN 'Verifyonly'
+  WHEN RH.restore_type = 'R' THEN 'Revert'
+  ELSE RH.restore_type 
+END AS Restore_Type,
+RH.[Replace],
+RH.[Recovery],
+RH.Restore_Date AS Restored_On,
+BMF.physical_device_name AS Restored_From,
+RF.destination_phys_name AS Current_DB_File_Location,
+RH.user_name AS Restored_By,
+BS.machine_name,
+BS.Server_Name
+FROM msdb.dbo.RestoreHistory RH 
+INNER JOIN msdb.dbo.BackupSet BS ON RH.backup_set_id = BS.backup_set_id
+INNER JOIN msdb.dbo.restorefile RF ON RH.Restore_History_id = RF.Restore_History_id
+INNER JOIN msdb.dbo.Backupmediafamily BMF ON bs.media_set_id = bmf.media_set_id
 )
 SELECT *
-FROM [LastRestores]
-WHERE [RowNum] = 1
+FROM MostRecentRestore
+WHERE [RowNum] = 1 AND destination_database_name = 'YourDatabaseName'
