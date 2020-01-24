9df46f679299a5eb9e05a94ef7dc45207672eff0
Tyrell66 <3510934+Tyrell66@users.noreply.github.com>
2020-01-24 14:45:08 +0100

    Update DatabaseList_Cursor.sql

--- DatabaseList_Cursor.sql
+++ DatabaseList_Cursor.sql
@@ -16,17 +16,14 @@ OPEN @DBListCursor;
 FETCH NEXT FROM @DBListCursor INTO @DBName, @db_id
  WHILE @@FETCH_STATUS = 0
 BEGIN
-	--PRINT CONVERT(nvarchar,@db_id)+' '+@DBName    -- Replace this line with the query you want to execute against all the DBs.
-	SET @SQL = 'USE [master]' + CHAR(13) + CHAR(10) + 
-	           'GO' + CHAR(13) + CHAR(10) + 
-			   CHAR(13) + CHAR(10) + 
-			   'ALTER DATABASE [' + @DBName + '] SET QUERY_STORE = ON ' + CHAR(13) + CHAR(10) + 
-			   'GO ' + CHAR(13) + CHAR(10) + 
-			   'ALTER DATABASE [' + @DBName + '] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, MAX_STORAGE_SIZE_MB = 200) ' + CHAR(13) + CHAR(10) + 
-			   'GO'
+	PRINT CONVERT(nvarchar,@db_id)+' '+@DBName    -- Replace this line with the query you want to execute against all the DBs.
+	--Remember NOT to use the GO statement in the @SQL. Prefer the ; instead.
+	SET @SQL = 'USE [master];' + CHAR(13) + CHAR(10) + 
+		   'ALTER DATABASE [' + @DBName + '] SET QUERY_STORE = ON;' + CHAR(13) + CHAR(10) + 
+		   'ALTER DATABASE [' + @DBName + '] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, MAX_STORAGE_SIZE_MB = 200);' + CHAR(13) + CHAR(10) + 
 	PRINT @SQL
-	--EXEC sp_executesql @SQL
-	--PRINT 'Next please...'
+	EXEC sp_executesql @SQL
+	PRINT 'Next please...'
 	FETCH NEXT FROM @DBListCursor INTO @DBName,@DB_id;
 END
 CLOSE @DBListCursor;
