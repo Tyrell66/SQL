4f1ca264191dbf79b50668abbeca82d2f6cbf1a5
Tyrell66 <3510934+Tyrell66@users.noreply.github.com>
2020-10-21 14:01:49 +0200

    Update GetLatestFromCommandLog.sql
    Selecting relevant columns only.

--- GetLatestFromCommandLog.sql
+++ GetLatestFromCommandLog.sql
@@ -1,4 +1,9 @@
-SELECT *
+SELECT DatabaseName,
+	   Command,
+	   StartTime,
+	   EndTime,
+	   ErrorNumber,
+	   ErrorMessage
   FROM [cs_helper].[dbo].[CommandLog]
   WHERE StartTime >= cast(dateadd(day, -2, getdate()) as date) 
 	and CommandType like 'BACKUP_DATABASE'
